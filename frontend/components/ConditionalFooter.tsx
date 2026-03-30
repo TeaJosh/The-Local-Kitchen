02513243c9a29e917836894c173ab990076b17ef
Tejosh Rana <tejoshrana@gmail.com>
2026-03-30 14:58:21 -0500

    fix: hide footer in blog post

--- frontend/components/ConditionalFooter.tsx
+++ frontend/components/ConditionalFooter.tsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function ConditionalFooter() {
   const pathname = usePathname();
-  const hideOn = ["/auth/login", "/auth/select-account", "/auth/register-member", "/auth/register-restaurant"];
+  const hideOn = ["/auth/login", "/auth/select-account", "/auth/register-member", "/auth/register-restaurant", "/blog/create-post"];
 
   if (hideOn.includes(pathname)) return null;
   return <Footer />;
